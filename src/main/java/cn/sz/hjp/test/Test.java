3ada000bcd67b5bd7592d6795d41cecfb0885498
MrHuang22 <3544121532@qq.com>
2020-06-10 20:48:16 +0800

    3

--- src/main/java/cn/sz/hjp/test/Test.java
+++ src/main/java/cn/sz/hjp/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("第一次提交Git测试!");
-		System.out.println("fuck you");
+		System.out.println("fuck you2332");
 	}
 	
 
